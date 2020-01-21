95c3193c47267cdf04aec361fcd021102291b625
Talbekk <mt.campbell@outlook.com>
2020-01-21 15:45:56 +0000

    initialize project with dependencies

---
+++ src/test/java/com/codeclan/example/fileservice/FileserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.fileservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
